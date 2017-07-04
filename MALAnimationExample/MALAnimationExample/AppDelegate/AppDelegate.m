d47a40aebf9829af28fa871369b0b3e5ac4ff9cb
maliang <295011445@qq.com>
2017-07-04 19:59:09 +0800

    ..

--- MALAnimationExample/MALAnimationExample/AppDelegate/AppDelegate.m
+++ MALAnimationExample/MALAnimationExample/AppDelegate/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <CHGlobalSingleton.h>
 
 @interface AppDelegate ()
 
@@ -18,6 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    [CHGlobalSingleton startNetMonitoring];
     return YES;
 }
 
---
+++ MALAnimationExample/Pods/Target Support Files/Pods-MALAnimationExample/Pods-MALAnimationExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MALAnimationExample : NSObject
+@end
+@implementation PodsDummy_Pods_MALAnimationExample
+@end
